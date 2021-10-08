f16dfd3248e02f34f127e2cce5aed4c912f12764
unparen.github.io <you@example.com>
2021-10-08 09:23:11 -0300

    microfix

--- example/doc-gen.conf.js
+++ example/doc-gen.conf.js
@@ -1,41 +1,6 @@
-import { finMd } from "@grandcore.org/docs";
-import genJsCode from "@grandcore.org/docs__gen-js-doc";
+// import { finMd } from "@grandcore.org/docs";
+// import genJsCode from "@grandcore.org/docs__gen-js-doc";
 
 export default {
-  // версия конфигурации
-  v: 1,
-  // режим работы менеджера
-  mode: "default",
-  // правила генерации файлов
-  rules: {
-    // Конфигурация по умолчанию для всех правил
-    default: {
-      // Сборщик конечного файла
-      finalizer: finMd({
-        mode: "default",
-      }),
-      // папка для сгенерированных файлов
-      output: "./dist/",
-    },
-    // Пример конфигурации (перекрывает стандартные настройки)
-    components: {
-      // папка с файлами для генерации
-      input: "./about/",
-      // маска для файлов
-      test: /\.jsx?$/,
-      // папка для сгенерированных файлов
-      output: "./dist/components/",
-      // генератор документации для правилаfinHtml
-      generator: genJsCode({
-        mode: "TypeScript",
-      }),
-      // мета-данные для файлов документации
-      meta: {
-        tags: "components, default",
-        template: "catalog",
-      },
-    },
-  },
-  // внешние скрипты, которые запускаются после успешного завершения генерации.
-  scripts: ["./site-gen.sh", "./tests.py"],
+a: 1
 };
--- main/index.js
+++ main/index.js
@@ -1,3 +1,5 @@
-import conf from "/doc-gen.conf.js";
+import conf from "../../../doc-gen.conf.js";
 
 console.log(conf);
+console.log(process.argv[2]);
+
