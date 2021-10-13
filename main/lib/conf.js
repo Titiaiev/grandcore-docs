30a5aec002f64ea369487c1ce446ecbc167e9ebd
unparen.github.io <you@example.com>
2021-10-13 16:37:18 -0300

    microfix

--- main/lib/conf.js
+++ main/lib/conf.js
@@ -1,14 +1,53 @@
+// есть ли аргумент проверяем наличие файла по нему и делаем динамический импорт, если файла нет, выбрасываем ошибку
+
+// если нет аргумента, проверяем есть ли стандартный файл конфига и делайем динамический импорт, если файла нет, выбрасываем ошибку
+
 import fs from "fs";
 import process from "process";
+let modulePath;
 
-if (process.argv[2] == true) {
-  let modulePath = process.argv[2];
-} else if (fs.existsSync("../../../../doc-gen.conf.js")) {
-  let modulePath = "../../../../doc-gen.conf.js";
+
+
+try{
+// Проверяем есть ли аргумент
+if (process.argv[2]) {
+  // Проверяем есть ли файл из аргумента
+  console.log(process.argv[2])
+  if (fs.existsSync(process.argv[2])) {
+    // если есть, добавляем в переменную
+    modulePath = process.argv[2];
+  }
+  // если нет - выбрасываем ошибку
+  else if (fs.existsSync('../doc-gen.conf.js')) {
+    modulePath = '../doc-gen.conf.js'
+  }
 } else {
-  throw new UserException("Не найден файл конфигурации");
+  console.log(fs.existsSync('conf.js'));
+  // throw "Не найден файл конфигурации";
+}
 }
 
-const conf = await import(modulePath);
+catch {
+  //  throw "Неизвестная ошибка";
+}
 
-export default conf;
+
+
+console.log(modulePath)
+// const conf = await import(modulePath);
+
+// export default conf;
+
+
+
+// let modulePath
+// if (process.argv[2]) modulePath = process.argv[2];
+// else if (fs.existsSync("../../example/doc-gen.conf.js")) modulePath = "../../example/doc-gen.conf.js"
+
+// // console.log(modulePath);
+// let conf = await import(modulePath);
+// export default conf;
+
+
+
+
